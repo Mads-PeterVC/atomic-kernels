0e37b95725ed10861a1b65fbdb7bdb4d9b1c0f4f
Mads-Peter <machri@phys.au.dk>
2026-02-12 17:27:24 +0100

    Added cells to molecules.

--- crates/ak-vis/examples/xyz/make_molecules.py
+++ crates/ak-vis/examples/xyz/make_molecules.py
@@ -6,6 +6,8 @@ from ase.collections import g2
 best_name = max(g2.names, key=lambda name: len(molecule(name)))
 print(f"The largest molecule in the G2 set is: {best_name}")
 
-# for name in g2.names:
-#     atoms = molecule(name)
-#     write(f"molecules/{name}.xyz", atoms)
+for name in g2.names:
+    atoms = molecule(name)
+    atoms.cell = [10, 10, 10]
+    atoms.center()
+    write(f"{name}.xyz", atoms)
