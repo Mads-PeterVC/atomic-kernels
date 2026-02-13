cfabff699781e7b3217741bc026e2c1012f0f30f
Mads-Peter <machri@phys.au.dk>
2026-02-14 00:20:26 +0100

    Slightly reduced radius.

--- crates/ak-vis/src/visuals/convert.rs
+++ crates/ak-vis/src/visuals/convert.rs
@@ -6,7 +6,7 @@ use bevy::prelude::*;
 pub fn convert_structure<C: ColorScheme>(view: &StructureView, scheme: &C) -> Vec<AtomVisual> {
     let mut visuals: Vec<AtomVisual> = Vec::with_capacity(view.len());
     for i in 0..view.positions.len() {
-        let radius: f32 = PERIODIC_TABLE.get(view.numbers[i]).covalent_radius as f32;
+        let radius: f32 = 0.9*PERIODIC_TABLE.get(view.numbers[i]).covalent_radius as f32;
         let color = scheme.color(&view, i);
         let atom_visual = AtomVisual::new(view.positions[i], color, radius);
         visuals.push(atom_visual);
